dad60b442fdb16e8250f24a49db6455870aa0229
Jason <jasonleetaylor88@gmail.com>
2020-11-27 20:49:31 +0000

    Base SI version 2.0

--- app.js
+++ app.js
@@ -1,125 +1,158 @@
-const canvas = document.querySelector("canvas");
-canvas.width = 960;
-canvas.height = 800;
+window.addEventListener("load", init);
+window.addEventListener("keydown", shooterMoveLeft);
+window.addEventListener("keydown", shooterMoveRight);
+window.addEventListener("keydown", shoot );
 
+
+const canvas = document.querySelector("canvas");
+canvas.height = 900;
+canvas.width = 900;
 const ctx = canvas.getContext("2d");
 
-const pixel = 10;
-const pixelGap = 1;
-const totalRows = canvas.height / pixel;
-const totalColumns = canvas.width / pixel;
-const background = "black";
-
 let grid = {};
-for (let i = 0; i < totalRows; i++) {
-    grid[`row${i}`] = {
-        rowTop: i * pixel,
-        columns: {}
-    };
-    for (let j = 0; j < totalColumns; j++){
-        grid[`row${i}`].columns[`column${j}`] = {
-            columnLeft: j * pixel,
-            color: background
-        };
+const gap = 1;
+const pixel =10;
+const actPixel = pixel - gap;
+const columns = canvas.width/pixel;
+const rows = canvas.height/pixel;
+const baseColor = "black";
+
+// Set up the properties of the base grid
+function baseGrid() {
+    for (let row = 0; row < rows ; row++ ) {
+        grid[`row${row}`] = {};
+        for (let column = 0; column < columns; column++) {
+            grid[`row${row}`][`column${column}`] = {
+                color: baseColor,
+                top: row*pixel,
+                left: column*pixel,
+            };
+        }
     }
 }
 
+// Function that iterates over the grid and draws
 grid.draw = function() {
-    for (let i = 0; i < totalRows; i++ ){
-        const top = grid[`row${i}`].rowTop;
-        for (let j = 0; j < totalColumns; j++) {
-            const left = grid[`row${i}`].columns[`column${j}`].columnLeft;
-            const fill = grid[`row${i}`].columns[`column${j}`].color;
-            ctx.fillStyle = fill;
-            ctx.fillRect(left, top, pixel-pixelGap, pixel-pixelGap);
+    for (let row = 0; row < rows; row++) {
+        for (let column = 0; column < columns; column++) {
+            ctx.fillStyle = this[`row${row}`][`column${column}`].color;
+            ctx.fillRect(this[`row${row}`][`column${column}`].left + gap, 
+            this[`row${row}`][`column${column}`].top + gap, actPixel, actPixel);
         }
     }
 }
-grid.clear = function() {
-    for (let i = 0; i < totalRows; i++) {
-        for (let j = 0; j < totalColumns ; j++ ) {
-            grid[`row${i}`].columns[`column${j}`].color = background;
-        }
-    }
+function init() {
+    canvas.height = 900;
+    canvas.width = 900;
+
+    // Create and draw initial blank grid
+    baseGrid();
+    grid.draw();
+
+    // Impose shooter on base grid and redraw grid
+    shooter.impose();
+    grid.draw();
 }
-let shooterLeft = 45;
-const shooterWidth = 10;
-const shooterTop = totalRows - 10;
-const shooterBottom = totalRows - 1;
-function Shooter(left, width, top, bottom) {
-    this.left = left;
-    this.width = width;
-    this.dx = 2;
-    this.draw = function() {
-        for (let i = top; i < bottom ; i++ ){
-            for (let j = this.left; j < this.left + this.width; j++ ){
-            grid[`row${i}`].columns[`column${j}`].color = "green";
+
+function Shooter(leftColumn, topRow, shooterColumns, rows, color, grid) {
+    this.x = leftColumn,
+    this.y = topRow,
+    this.columns = shooterColumns,
+    this.rows = rows, 
+    this.color = color,
+    this.dx = shooterColStep,
+    this.impose = function() {
+        for (let row = this.y; row < this.y + this.rows; row++) {
+            for (let column = this.x; column < this.x + this.columns ; column++) {
+                grid[`row${row}`][`column${column}`].color = this.color;
             }
         }
     }
-    this.moveLeft = function() {
-        if (this.left - this.dx <= 0) {
-            this.left = 0;
-        } else {
-            this.left -= this.dx;
-        }
-        shooterLeft = this.left;
-    }
-    this.moveRight = function() {
-        if (this.left + this.width + this.dx >= totalColumns) {
-            this.left = totalColumns - this.width;
-        } else {
-            this.left += this.dx;
-        }
-        shooterLeft = this.left;
-    }    
-};
 
-function Shot(x, y) {
-    this.left = x + 4;
-    this.top = y - 1 ;
-    this.count = 0;
-    this.shoot = function() {
-        grid[`row${this.top}`].columns[`column${this.left}`].color = "green";
+    this.moveLeft = function() {
+        if (this.x - this.dx > 0) {
+            this.x -= this.dx;
+        } else {
+            this.x = 0;
+        }
+    }
+
+    this.moveRight = function() {
+        if (this.x + this.dx < columns - this.columns) {
+            this.x += this.dx;
+        } else {
+            this.x = columns - this.columns;
+        }
+    }
+
+}
+
+function Shot(shooterX, shooterY, shooterColumns, shotColor, grid) {
+    this.x = shooterX,
+    this.y =shooterY,
+    this.columns = shooterColumns,
+    this.shotColor = shotColor,
+    this.dy = 1,
+    this.bullet = function() {
+        grid[`row${this.y - 1}`][`column${this.x + (this.columns-1)/2}`].color = this.shotColor;
     }
     this.update = function() {
-        this.shoot();
-        if (this.count % 2 == 0 && this.count != 0 && this.top - 1 >= 0) {
-            this.top -= 1; 
-        } else if (this.count % 2 == 0 && this.count != 0 && this.top - 1 < 0) {
+        this.bullet();
+            
+        if (this.y - this.dy > 0) {
+            this.y -= this.dy;
+        } else {
             shots.splice(shots.indexOf(this), 1);
         }
-        this.count++;
     }
 }
-shooter = new Shooter(shooterLeft, shooterWidth, shooterTop, shooterBottom);
-shots = [];
+
+
+// Initial shooter definition
+const shooterCols = 21;
+const shooterRows = 10;
+const shooterGap = 2;
+const shooterColStart = (columns/2) - (shooterCols-1)/2;
+const shooterRowStart = (rows - shooterRows - shooterGap); 
+const shooterColor = "yellow";
+const shooterColStep = 1;
+const shotColor = "red";
+let shooter = new Shooter(shooterColStart, shooterRowStart, shooterCols, shooterRows, shooterColor, grid);
+
+
+function shooterMoveLeft(e) {
+    if (e.key == "ArrowLeft") {
+        shooter.moveLeft();
+        shooter.impose();
+    }
+}
+
+function shooterMoveRight(e) {
+    if (e.key == "ArrowRight") {
+        shooter.moveRight();
+        shooter.impose();
+    }
+}
+let shots =[];
+function shoot(e) {
+    if (e.code == "Space") {
+        shot = new Shot(shooter.x, shooter.y, shooter.columns, shotColor, grid);
+        shot.update();
+        shots.push(shot);
+    }
+}
+
+
 
 function animate() {
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    grid.clear();
-    shooter.draw();
-    for (let i = 0; i < shots.length; i++) {
+    baseGrid();
+    shooter.impose();
+    for (let i = 0; i < shots.length ; i++) {
         shots[i].update();
     }
     grid.draw();
-
 }
 
-
-function shooterMove(e) {
-    if (e.key == "ArrowLeft") {
-        shooter.moveLeft();
-    } else if (e.key == "ArrowRight") {
-        shooter.moveRight();
-    }
-}
-function newShot(e) {
-    if (e.code == "Space") {
-        shots.push(new Shot(shooterLeft, shooterTop));
-    }
-}
-window.addEventListener("keydown", shooterMove);
-window.addEventListener("keydown", newShot);
-window.addEventListener("load", animate);
+animate(); 
